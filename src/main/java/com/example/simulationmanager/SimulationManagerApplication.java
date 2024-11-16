4380ad180ab7db4474c4f52a935757f08d30e896
Mark Veerasingam <markveer70@gmail.com>
2024-11-16 01:10:58 +0000

    first commit

---
+++ src/main/java/com/example/simulationmanager/SimulationManagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.simulationmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimulationManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimulationManagerApplication.class, args);
+    }
+
+}
