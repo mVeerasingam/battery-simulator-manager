25c3a0a480ef38cffb45ac4159109089749e8c8b
Mark Veerasingam <markveer70@gmail.com>
2024-12-29 19:22:10 +0000

    added comments to the dto

--- src/main/java/com/example/simulationmanager/DTO/SimulationRequest.java
+++ src/main/java/com/example/simulationmanager/DTO/SimulationRequest.java
@@ -4,9 +4,9 @@ import java.util.List;
 import java.util.Map;
 
 public class SimulationRequest {
-    private Map<String, Object> equivalentCircuitModel;
-    private Map<String, Object> parameterValues;
-    private Map<String, Object> solver;
-    private Map<String, Object> simulation;
-    private List<String> displayParams;
+    private Map<String, Integer> equivalentCircuitModel; // { "RC_pairs": 1 }
+    private Map<String, String> parameterValues;         // { "parameter_value": "ECM_Example" }
+    private Map<String, String> solver;                 // { "solver": "IDAKLUSolver" }
+    private Map<String, Object> simulation;             // flexible enough to support main post body of simTypes https://github.com/MarkVeerasingam/Battery-Simulator/tree/main?tab=readme-ov-file#performing-various-simultion-types:~:text=Performing%20various%20simultion%20types
+    private List<String> displayParams;                 // ["Voltage [V]", "Current [A]", ...]
 }
