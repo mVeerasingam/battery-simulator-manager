af9e302eb630f12df79cf2febf0741f4e558b0e6
Mark Veerasingam <markveer70@gmail.com>
2024-12-29 19:11:35 +0000

    updated dto to make more sense

--- src/main/java/com/example/simulationmanager/DTO/SimulationRequest.java
+++ src/main/java/com/example/simulationmanager/DTO/SimulationRequest.java
@@ -1,9 +1,12 @@
 package com.example.simulationmanager.DTO;
 
+import java.util.List;
 import java.util.Map;
 
 public class SimulationRequest {
-    private String taskId;
-    private String simulationType;
-    private Map<String, Object> simulationConfig; // Simulation configuration details (parameters, solver, etc.)
+    private Map<String, Object> equivalentCircuitModel;
+    private Map<String, Object> parameterValues;
+    private Map<String, Object> solver;
+    private Map<String, Object> simulation;
+    private List<String> displayParams;
 }
