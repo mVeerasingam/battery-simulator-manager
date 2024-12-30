4b965da70107d2b595b427d831ee7e9113d42bb8
Mark Veerasingam <markveer70@gmail.com>
2024-12-30 16:43:37 +0000

    422 unprocessable entity i have a headache

--- src/main/java/com/example/simulationmanager/SimulationClient.java
+++ src/main/java/com/example/simulationmanager/SimulationClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "battery-simulation-api", url = "http://localhost:8084")
+@FeignClient(name = "battery-simulation-api", url = "http://battery_simulator:8084")
 public interface SimulationClient {
     @PostMapping("/simulate/ecm")
     String runSimulation(@RequestBody SimulationRequest simulationRequest);
